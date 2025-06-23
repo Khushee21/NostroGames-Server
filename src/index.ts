a3af93e91a1b47990d06ed18959dde4a61a9f273
Khushi Rathore <khushirathore64@example.com>
2025-06-23 18:40:13 +0530

    auth done

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './utils/db';
 import logger from './middleware/logger';
+import AuthRouter from './routes/Auth.routes';
 
 dotenv.config();
 
@@ -11,14 +12,14 @@ async function bootstrap() {
     //DB connection
     await connectDB();
 
-    const app= express();
-    const PORT=process.env.PORT || 8000;
+    const app = express();
+    const PORT = process.env.PORT || 8000;
 
     //ENABLE CORS
-    const corsOrigin= process.env.CORS_ORIGIN;
+    const corsOrigin = process.env.CORS_ORIGIN;
 
     app.use(cors({
-        origin:corsOrigin,
+        origin: corsOrigin,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
@@ -26,24 +27,25 @@ async function bootstrap() {
 
     //BODY PARSERS
     app.use(express.json());
-    app.use(express.urlencoded({ extended: true}));
+    app.use(express.urlencoded({ extended: true }));
 
     //LOGGING
     app.use(logger)
 
     //ROUTES
 
-    app.get('/',(req , res)=>{
-        res.send({ message: 'Nostrogame server is running!'});
+    app.get('/', (req, res) => {
+        res.send({ message: 'Nostrogame server is running!' });
     })
+    app.use('/Auth', AuthRouter);
 
-    app.use((err: any , _req: Request , res: Response , _next: NextFunction)=>{
+    app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
         console.log(err);
-        res.status(500).send({ error: 'Something went wrong'});
+        res.status(500).send({ error: 'Something went wrong' });
     })
 
     //start listening
-    app.listen(PORT ,()=>{
+    app.listen(PORT, () => {
         console.log(`Nostrogame server running on ${PORT}`)
     });
 }
